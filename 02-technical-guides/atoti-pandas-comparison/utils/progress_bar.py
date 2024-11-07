427a7549824636c0af5ffbd508e780f264e1e757
Andrew Yang <aya@aya.local>
2024-11-07 14:05:33 -0500

    linting and reverting pandas-main code to use S3

--- 02-technical-guides/atoti-pandas-comparison/utils/progress_bar.py
+++ 02-technical-guides/atoti-pandas-comparison/utils/progress_bar.py
@@ -1,6 +1,6 @@
 # Define a function to print a progress bar
-def print_progress_bar(iteration, total, prefix='', suffix='', length=50, fill='█'):
+def print_progress_bar(iteration, total, prefix="", suffix="", length=50, fill="█"):
     percent = f"{100 * (iteration / float(total)):.1f}"
     filled_length = int(length * iteration // total)
-    bar = fill * filled_length + '-' * (length - filled_length)
-    print(f'\r{prefix} |{bar}| {percent}% {suffix}', end='\r')
+    bar = fill * filled_length + "-" * (length - filled_length)
+    print(f"\r{prefix} |{bar}| {percent}% {suffix}", end="\r")

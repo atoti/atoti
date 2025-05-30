838625c183b78ad1b583538c78fc425b7fbd6972
Andrew Yang <aya@aya.local>
2025-05-29 23:02:32 -0400

    adding logic to reduce number of workers for macos-13 GHA runner by 1

--- tests/test_notebooks.py
+++ tests/test_notebooks.py
@@ -1,9 +1,16 @@
 from exclusion_utils import get_included_notebooks
 import pytest
 import sys
+import platform
+import os
+
+# Reduce number of workers on macos-13 GitHub Action runner to avoid CPU overload
+if platform.system() == "Darwin" and platform.release() == "22.6.0":
+    num_workers = os.cpu_count() - 1
+else:
+    num_workers = "auto"
 
 notebooks = get_included_notebooks()
-
 for notebook in notebooks:
     print(notebook)
 
@@ -12,7 +19,7 @@ if __name__ == "__main__":
         "--nbmake",
         "--nbmake-timeout=600",
         "-n",
-        "auto",
+        f"{num_workers}",
         "--dist",
         "worksteal",
         "-v",
